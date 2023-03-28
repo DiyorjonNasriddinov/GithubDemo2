a2ab3b61bdabf408b0fc7ba636e009791d3a3b26
Diyorjon Nasriddinov <diyorjonnasriddinov@gmai.com>
2023-03-28 12:37:27 +0500

    Initial Commit

---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Diyorjon Nasriddinov on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
